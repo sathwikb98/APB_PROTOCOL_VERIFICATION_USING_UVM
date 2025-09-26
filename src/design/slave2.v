0da5811e36da4c164ff1aaa9f05d6484fa2eed87
Sathwik Bhat <109812189+sathwikb98@users.noreply.github.com>
2025-09-26 09:46:38 +0530

    Update slave2.v

--- src/design/slave2.v
+++ src/design/slave2.v
@@ -1,38 +1,39 @@
 `timescale 1ns/1ns
- 
+
+
+
 module slave2(
          input PCLK,PRESETn,
          input PSEL,PENABLE,PWRITE,
          input [7:0]PADDR,PWDATA,
-         output [7:0]PRDATA2,
-         output reg PREADY );
- 
+        output [7:0]PRDATA2,
+        output reg PREADY );
+
      reg [7:0]reg_addr;
- 
-     reg [7:0] mem2 [0:63];
- 
+
+     reg [7:0] mem2 [0:255];
+
     assign PRDATA2 =  mem2[reg_addr];
- 
- 
+
     always @(*)
        begin
          if(!PRESETn)
               PREADY = 0;
           else
-          if(PSEL && !PENABLE && !PWRITE)
-             begin PREADY = 0; end
- 
-          else if(PSEL && PENABLE && !PWRITE)
-             begin  PREADY = 1;
+    if(PSEL && !PENABLE && !PWRITE)
+       begin PREADY = 0; end
+
+    else if(PSEL && PENABLE && !PWRITE)
+       begin  PREADY = 1;
                     reg_addr =  PADDR;
-               end
-          else if(PSEL && PENABLE && PWRITE)
-             begin  PREADY = 0; end
- 
-          else if(PSEL && PENABLE && PWRITE)
-             begin  PREADY = 1;
-                    mem2[PADDR] = PWDATA; end
- 
+         end
+          else if(PSEL && !PENABLE && PWRITE)
+       begin  PREADY = 0; end
+
+    else if(PSEL && PENABLE && PWRITE)
+       begin  PREADY = 1;
+              mem2[PADDR] = PWDATA; end
+
            else PREADY = 0;
         end
     endmodule
